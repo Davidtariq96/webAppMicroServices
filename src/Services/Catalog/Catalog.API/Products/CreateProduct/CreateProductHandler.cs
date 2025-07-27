4766775fb31bab987f95dc0cba80a35f5b264bac
Davidtariq96 <samikendrickjnr@gmail.com>
2025-07-27 18:59:34 +0100

    add stores

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,10 @@
+using MediatR;
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+: IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+//Add Services to container
+var app = builder.Build();
+
+
+//Configure HTTPS REQUEST PIPELLINES
+
+app.Run();
